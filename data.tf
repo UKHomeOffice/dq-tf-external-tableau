e8d11bd932abc28036d7c7f0c654188bf76c8b62
david-wirelab <info@wirelab.co.uk>
2019-02-28 15:28:01 +0000

    remove win instances

--- data.tf
+++ data.tf
@@ -1,21 +1,3 @@
-data "aws_region" "current" {}
-
-data "aws_ami" "ext_tableau" {
-  most_recent = true
-
-  filter {
-    name = "name"
-
-    values = [
-      "dq-tableau-7*",
-    ]
-  }
-
-  owners = [
-    "self",
-  ]
-}
-
 data "aws_ami" "ext_tableau_linux" {
   most_recent = true
 
