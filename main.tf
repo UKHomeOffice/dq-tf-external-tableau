fb191968ca3e20e3387a61ab70a0bd2ab359d4ff
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-03-04 11:48:10 +0000

    Dup sg

--- main.tf
+++ main.tf
@@ -219,14 +219,3 @@ resource "aws_security_group_rule" "allow_lambda" {
 
   security_group_id = "${aws_security_group.sgrp.id}"
 }
-
-resource "aws_security_group_rule" "allow_out" {
-  type            = "egress"
-  from_port       = 0
-  to_port         = 0
-  protocol        = -1
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = "${aws_security_group.sgrp.id}"
-}
-
