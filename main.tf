8a54d2a194da016d646f5da3b36edc7d505de8ad
Duncan Mountford <duncan9562@gmail.com>
2019-06-17 15:55:25 +0100

    Add NotProd instance

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ locals {
 }
 
 resource "aws_instance" "ext_tableau_linux" {
-  count                       = "${var.environment == "prod" ? "2" : "0"}"                       # Allow different instance count in prod and notprod
+  count                       = "${var.environment == "prod" ? "2" : "1"}"                       # Allow different instance count in prod and notprod
   key_name                    = "${var.key_name}"
   ami                         = "${data.aws_ami.ext_tableau_linux.id}"
-  instance_type               = "c5.4xlarge"
+  instance_type               = "${var.environment == "prod" ? "c5.4xlarge" : "c5.2xlarge"}"
   iam_instance_profile        = "${aws_iam_instance_profile.ext_tableau.id}"
   vpc_security_group_ids      = ["${aws_security_group.sgrp.id}"]
   associate_public_ip_address = false
