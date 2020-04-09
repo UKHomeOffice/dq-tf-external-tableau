918ff4d249b7cfb5bcefa07a06bb129d8e697d05
Thet Naing <thet.naing@digital.homeoffice.gov.uk>
2020-04-09 13:14:00 +0100

    last confirmed by praveen for logo

--- main.tf
+++ main.tf
@@ -332,12 +332,14 @@ echo "#TSMCMD - initial user"
 tabcmd initialuser --server 'localhost:80' --username "$TAB_ADMIN_USER" --password "$TAB_ADMIN_PASSWORD"
 
 # DQdashboard Customise
-export DASH_IMAGE="$DATA_ARCHIVE_TAB_BACKUP_URL/DQDashboards/Image/DQDashboards_Image_Home_Office_new-01.png"
-aws s3 cp $DASH_IMAGE /tmp
-tsm customize --server-name "HO External Carrier Portal"
-tsm customize --signin-logo /tmp/Home_Office_new-01.png
-tsm customize --logo /tmp/Home_Office_new-01.png
-tsm customize --header-logo /tmp/Home_Office_new-01.png
+export LOGO=DQDashboards_Image_Home_Office_new-01.png
+export DASH_IMAGE="$DATA_ARCHIVE_TAB_BACKUP_URL/DQDashboards/Image/$LOGO"
+export TMP_FOLDER=/tmp
+aws s3 cp $DASH_IMAGE $TMP_FOLDER
+tsm customize --server-name "HO External Carrier Portal" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
+tsm customize --signin-logo /$TMP_FOLDER/$LOGO
+tsm customize --logo /$TMP_FOLDER/$LOGO
+tsm customize --header-logo /$TMP_FOLDER/$LOGO
 tsm data-access repository-access enable --repository-username $TAB_TABSVR_REPO_USER --repository-password $TAB_TABSVR_REPO_PASSWORD --ignore-prompt
 
 # Always restore from green
