b6afa356fc2d9008e5d843a6d099507b1e6a61d4
Thet Naing <thet.naing@digital.homeoffice.gov.uk>
2020-05-01 12:07:19 +0100

    increase only prod instance size

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "ext_tableau_linux" {
   count                       = "${var.environment == "prod" ? "2" : "1"}"                       # 2 in Prod (Green & Blue), 1 in NotProd (Green only)
   key_name                    = "${var.key_name}"
   ami                         = "${data.aws_ami.ext_tableau_linux.id}"
-  instance_type               = "c5.4xlarge"
+  instance_type               = "${var.environment == "prod" ? "c5.4xlarge" : "c5.2xlarge"}"
   iam_instance_profile        = "${aws_iam_instance_profile.ext_tableau.id}"
   vpc_security_group_ids      = ["${aws_security_group.sgrp.id}"]
   associate_public_ip_address = false
@@ -199,10 +199,10 @@ EOF
 }
 
 resource "aws_instance" "ext_tableau_linux_staging" {
-  count                       = "${var.environment == "prod" ? "1" : "1"}"         # 1 in Prod, 0 in NotProd
+  count                       = "${var.environment == "prod" ? "1" : "1"}"                   # 1 in Prod, 0 in NotProd
   key_name                    = "${var.key_name}"
   ami                         = "${data.aws_ami.ext_tableau_linux_upgrade.id}"
-  instance_type               = "c5.4xlarge"
+  instance_type               = "${var.environment == "prod" ? "c5.4xlarge" : "c5.2xlarge"}"
   iam_instance_profile        = "${aws_iam_instance_profile.ext_tableau.id}"
   vpc_security_group_ids      = ["${aws_security_group.sgrp.id}"]
   associate_public_ip_address = false
