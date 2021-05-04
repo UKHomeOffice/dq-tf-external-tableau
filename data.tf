e6ccf0600cfe38ae297cdec3388cc8f1c80450bb
JWanderema <wanderema@hotmail.co.uk>
2021-05-04 11:53:47 +0100

    add cloudwatch monitoring

--- data.tf
+++ data.tf
@@ -5,7 +5,7 @@ data "aws_ami" "ext_tableau_linux" {
     name = "name"
 
     values = [
-      "dq-tableau-linux-218*",
+      "dq-tableau-linux-231*",
     ]
   }
 
--- main.tf
+++ main.tf
@@ -3,6 +3,14 @@ locals {
   naming_suffix_linux = "ext-tableau-linux-${var.naming_suffix}"
 }
 
+module "ec2_alarms_ext_tableau" {
+  source          = "github.com/UKHomeOffice/dq-tf-cloudwatch-ec2"
+  naming_suffix   = local.naming_suffix
+  environment     = var.environment
+  pipeline_name   = "ext-tableau"
+  ec2_instance_id = aws_instance.ext_tableau_linux[0].id
+}
+
 resource "aws_instance" "ext_tableau_linux" {
   count                       = var.environment == "prod" ? "2" : "1" # 2 in Prod (Green & Blue), 1 in NotProd (Green only)
   key_name                    = var.key_name
@@ -22,6 +30,9 @@ set -e
 
 #log output from this user_data script
 exec > >(tee /var/log/user-data.log|logger -t user-data ) 2>&1
+
+# start the cloud watch agent
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -s -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json
 
 echo "#Mount filesystem - /var/opt/tableau/"
 mkfs.xfs /dev/nvme2n1
