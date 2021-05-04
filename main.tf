7eff056ee07d4b51fd76a2f3a95b2d649ce7eb55
JWanderema <wanderema@hotmail.co.uk>
2021-05-04 23:44:24 +0100

    enable cw module

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ locals {
   naming_suffix_linux = "ext-tableau-linux-${var.naming_suffix}"
 }
 
-# module "ec2_alarms_ext_tableau" {
-#   source          = "github.com/UKHomeOffice/dq-tf-cloudwatch-ec2"
-#   naming_suffix   = local.naming_suffix
-#   environment     = var.environment
-#   pipeline_name   = "ext-tableau"
-#   ec2_instance_id = aws_instance.ext_tableau_linux[0].id
-# }
+module "ec2_alarms_ext_tableau" {
+  source          = "github.com/UKHomeOffice/dq-tf-cloudwatch-ec2"
+  naming_suffix   = local.naming_suffix
+  environment     = var.environment
+  pipeline_name   = "ext-tableau"
+  ec2_instance_id = aws_instance.ext_tableau_linux[0].id
+}
 
 resource "aws_instance" "ext_tableau_linux" {
   count                       = var.environment == "prod" ? "2" : "1" # 2 in Prod (Green & Blue), 1 in NotProd (Green only)
