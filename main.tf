89a3ee07cc8c8a11c7cdd50ca9b17ebc7dd91ed6
JWanderema <wanderema@hotmail.co.uk>
2021-05-04 16:21:49 +0100

    Fix - Terraform's validator understands that count.index is controlled by the count

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "ec2_alarms_ext_tableau" {
   naming_suffix   = local.naming_suffix
   environment     = var.environment
   pipeline_name   = "ext-tableau"
-  ec2_instance_id = aws_instance.ext_tableau_linux[0].id
+  ec2_instance_id = aws_instance.ext_tableau_linux[count.index].id
 }
 
 resource "aws_instance" "ext_tableau_linux" {
