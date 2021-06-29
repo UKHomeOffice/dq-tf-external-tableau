cae59c60313def0f5b3273d5a962f495f73ef5e7
JWanderema <wanderema@hotmail.co.uk>
2021-06-29 14:55:57 +0100

    add cw resource

--- alarms/variable.tf
+++ alarms/variable.tf
@@ -18,7 +18,7 @@ variable "path_module" {
 }
 
 variable "ec2_instance_id_0" {
-  default     = aws_instance.instance_cw[0].id
+  default     = "aws_instance.instance_cw[0]"
   description = "The instance ID of the RDS database instance that you want to monitor."
   type        = string
 }
