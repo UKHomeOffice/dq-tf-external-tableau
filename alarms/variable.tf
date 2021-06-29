c9e8020a5042d1389f14ac920c5af639873ae317
JWanderema <wanderema@hotmail.co.uk>
2021-06-29 14:28:00 +0100

    add alarms

---
+++ alarms/variable.tf
@@ -0,0 +1,42 @@
+variable "environment" {
+}
+
+variable "naming_suffix" {
+}
+
+variable "pipeline_name" {
+  default = external_tableau_alarms
+}
+
+variable "swap_alarm" {
+  description = "Switch to turn off Swap monitoring (required for MSSQL). Accepted values are 'false' to turn off and 'true' to excplicitly turn on"
+  default     = "true"
+}
+
+variable "path_module" {
+  default = "unset"
+}
+
+variable "ec2_instance_id_0" {
+  default     = aws_instance.instance_cw[0].id
+  description = "The instance ID of the RDS database instance that you want to monitor."
+  type        = string
+}
+
+variable "cpu_utilization_threshold" {
+  description = "The maximum percentage of CPU utilization."
+  type        = string
+  default     = 80
+}
+
+variable "available_memory_threshold" {
+  description = "The percentage of available memory."
+  type        = string
+  default     = 20
+}
+
+variable "used_storage_space_threshold" {
+  description = "The minimum amount of available storage space in Byte."
+  type        = string
+  default     = 80
+}
