6ce8a0a3b016eeba53844e5757bdf701a1c34b5a
JWanderema <wanderema@hotmail.co.uk>
2021-06-29 16:49:14 +0100

    change structure

--- alarms.tf
+++ alarms.tf
@@ -1,6 +1,6 @@
 locals {
-  naming_suffix = "${var.ec2_instance_id_0}-${var.naming_suffix}"
-  path_module   = var.path_module != "unset" ? var.path_module : path.module
+  alarm_naming_suffix = "testing_alarm_naming_suffix"
+  path_module         = var.path_module != "unset" ? var.path_module : path.module
 
   thresholds = {
     CPUUtilizationThreshold   = min(max(var.cpu_utilization_threshold, 0), 100)
@@ -24,11 +24,11 @@ resource "aws_cloudwatch_metric_alarm" "cpu_utilization_too_high" {
   ok_actions          = [aws_sns_topic.ec2.arn]
 
   dimensions = {
-    InstanceId = aws_instance.ext_tableau_linux.id
+    InstanceId = aws_instance.ext_tableau_linux[0].id
   }
 
   depends_on = [
-    var.ec2_instance_id_0
+    [aws_instance.ext_tableau_linux[0]]
   ]
 
 }
@@ -47,11 +47,11 @@ resource "aws_cloudwatch_metric_alarm" "available_memory_too_low" {
   ok_actions          = [aws_sns_topic.ec2.arn]
 
   dimensions = {
-    InstanceId = "${var.ec2_instance_id_0}.id"
+    InstanceId = aws_instance.ext_tableau_linux[0].id
   }
 
   depends_on = [
-    var.ec2_instance_id_0
+    [aws_instance.ext_tableau_linux[0]]
   ]
 }
 
@@ -69,13 +69,13 @@ resource "aws_cloudwatch_metric_alarm" "Used_storage_space" {
   ok_actions          = [aws_sns_topic.ec2.arn]
 
   dimensions = {
-    InstanceId = "${var.ec2_instance_id_0}.id,"
+    InstanceId = aws_instance.ext_tableau_linux[0].id,
     path       = "/",
     fstype     = "xfs",
   }
 
   depends_on = [
-    var.ec2_instance_id_0
+    [aws_instance.ext_tableau_linux[0]]
   ]
 }
 #
@@ -93,10 +93,104 @@ resource "aws_cloudwatch_metric_alarm" "Used_storage_space" {
 #   ok_actions          = [aws_sns_topic.ec2.arn]
 #
 #   dimensions = {
-#     InstanceId = "${var.ec2_instance_id_0}.id"
+#     InstanceId = aws_instance.ext_tableau_linux[0].id
 #   }
 #
 #   depends_on = [
-#     var.ec2_instance_id_0
+#     [aws_instance.ext_tableau_linux[0]]
 #   ]
 # }
+
+
+##########
+#  Data  #
+##########
+data "aws_region" "current" {
+}
+
+data "aws_caller_identity" "current" {
+}
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+  policy_id = "__ec2_policy_ID"
+
+  statement {
+    sid = "__ec2_statement_ID"
+
+    actions = [
+      "SNS:Subscribe",
+      "SNS:SetTopicAttributes",
+      "SNS:RemovePermission",
+      "SNS:Receive",
+      "SNS:Publish",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:GetTopicAttributes",
+      "SNS:DeleteTopic",
+      "SNS:AddPermission",
+    ]
+
+    effect    = "Allow"
+    resources = [aws_sns_topic.ec2.arn]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+
+      values = [
+        data.aws_caller_identity.current.account_id,
+      ]
+    }
+  }
+
+  statement {
+    sid       = "Allow CloudwatchEvents"
+    actions   = ["sns:Publish"]
+    resources = [aws_sns_topic.ec2.arn]
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+  }
+
+  statement {
+    sid       = "Allow EC2 Event Notification"
+    actions   = ["sns:Publish"]
+    resources = [aws_sns_topic.ec2.arn]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+#########
+# SNS   #
+#########
+
+resource "aws_sns_topic" "ec2" {
+  name = var.pipeline_name
+}
+
+
+resource "aws_sns_topic_policy" "ec2" {
+  arn    = aws_sns_topic.ec2.arn
+  policy = data.aws_iam_policy_document.sns_topic_policy.json
+}
+
+resource "aws_sns_topic_subscription" "sns_to_lambda" {
+  topic_arn = aws_sns_topic.ec2.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.lambda_slack.arn
+}
+
+output "sns_topic_arn" {
+  description = "The ARN of the SNS topic"
+  value       = aws_sns_topic.ec2.arn
+}
