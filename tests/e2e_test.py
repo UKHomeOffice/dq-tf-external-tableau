9427865d9e5e9ce49846f0ab9879c282bccff184
haitham.abou-houly <haitham.abou-houly@digital.homeoffice.gov.uk>
2022-03-30 15:48:53 +0100

    update tableau version

--- tests/e2e_test.py
+++ tests/e2e_test.py
@@ -27,6 +27,7 @@ class TestE2E(unittest.TestCase):
               s3_archive_bucket            = "bucket-name"
               s3_archive_bucket_key        = "1234567890"
               s3_archive_bucket_name       = "bucket-name"
+              haproxy_private_ip           = "1.2.3.3"
               haproxy_private_ip2          = "1.2.3.3"
               haproxy_config_bucket        = "s3-bucket-name"
               haproxy_config_bucket_key    = "arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab"
