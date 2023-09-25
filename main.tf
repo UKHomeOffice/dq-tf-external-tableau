21f61534d93e616a6afbf4a5f8772f030cef9052
Srikanth Arunachalam <116078089+s-arunachalam@users.noreply.github.com>
2023-09-25 17:13:22 +0100

    fix: yel-8016p added enforce imdsv2 (#261)

--- main.tf
+++ main.tf
@@ -23,6 +23,9 @@ set -e
 #log output from this user_data script
 exec > >(tee /var/log/user-data.log|logger -t user-data ) 2>&1
 
+echo "Enforcing imdsv2 on ec2 instance"
+curl http://169.254.169.254/latest/meta-data/instance-id | xargs -I {} aws ec2 modify-instance-metadata-options --instance-id {} --http-endpoint enabled --http-tokens required
+
 # start the cloud watch agent
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -s -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json
 
