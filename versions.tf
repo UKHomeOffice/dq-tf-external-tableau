86717c0d5e28a991e77d78a33cfd473243c84882
Gurukishore Bolisetty <gurukishore.bolisetty1@digital.homeoffice.gov.uk>
2024-11-04 16:19:42 +0000

    updated terragrunt and terraform version

--- versions.tf
+++ versions.tf
@@ -1,4 +1,18 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.4.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.12.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
 }
