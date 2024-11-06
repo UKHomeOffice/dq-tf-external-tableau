00978eb82b38066fc2940dd72e04e276b2cf75b0
Gurukishore Bolisetty <gurukishore.bolisetty1@digital.homeoffice.gov.uk>
2024-11-06 12:40:45 +0000

    revert archive version

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     archive = {
       source  = "hashicorp/archive"
-      version = "2.4.1"
+      version = "~> 2.4.0"
     }
     aws = {
       source  = "hashicorp/aws"
